9e3a7e9ba5bad080ad78eaa66540f6e36ce3de36
wych42 <wych101010@gmail.com>
2020-10-20 00:04:44 +0800

    update template

--- src/main/java/io/xiatiao/catbin/catbin/CodeController.java
+++ src/main/java/io/xiatiao/catbin/catbin/CodeController.java
@@ -3,27 +3,27 @@ package io.xiatiao.catbin.catbin;
 import com.google.common.base.Strings;
 import com.google.common.hash.Hashing;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 @Controller
 public class CodeController {
     @Autowired
     private CustomCodeRepository codeRepository;
 
-    @GetMapping(value="/")
-    public String index(WebRequest request, Model model){
+    private String getUrl(WebRequest request) {
         String url = ((ServletWebRequest) request).getRequest().getRequestURL().toString();
         String protocol = request.getHeader("x-forwarded-proto");
         if (Strings.isNullOrEmpty(protocol)) {
@@ -39,28 +39,56 @@ public class CodeController {
             protocol = "http";
         }
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url);
-        url =  builder.scheme(protocol).toUriString();
+        url = builder.scheme(protocol).toUriString();
+        return url;
+    }
+
+    @GetMapping(value = "/")
+    public String index(WebRequest request, Model model) {
+        String url = getUrl(request);
         model.addAttribute("url", url);
         return "index";
     }
 
+    @GetMapping(value = {"/{id:\\d+}", "/{id:\\d+}/{syntax:[a-z]+}"})
+    public String index(WebRequest request, Model model,
+                        HttpServletResponse response,
+                        @PathVariable Integer id,
+                        @PathVariable(required = false) String syntax) {
+        Optional<Code> c = codeRepository.findById(id);
+        if (c.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, id.toString() + " not found");
+        }
+        System.out.println("syntax:"+syntax);
+        if (syntax == null) {
+            model.addAttribute("code", c.get().getContent());
+            HttpHeaders rh = new HttpHeaders();
+            response.addHeader("Content-Type", "text/plain; charset=utf-8");
+            return "plaincode";
+        }
+        model.addAttribute("syntax", syntax);
+        model.addAttribute("code", c.get().getContent());
+        return "code";
+    }
+
     @PostMapping(path = "/")
     @ResponseBody
-    public ResponseEntity<String> index(@RequestParam(name="cat") String code) {
+    public ResponseEntity<String> index(WebRequest request, @RequestParam(name = "cat") String code) {
         if (code.length() < 23 || code.length() > 64 * 1024) {
             return new ResponseEntity<>(
                     "the code snippet must be longer than 'print(\"Hello, world!\")' or shorter than 64 KiB.",
                     HttpStatus.PAYLOAD_TOO_LARGE);
         }
+        String url = getUrl(request);
         String digest = Hashing.sha256().hashString(code, StandardCharsets.UTF_8).toString();
         Code row = codeRepository.findByDigest(digest);
         if (row != null) {
-            return new ResponseEntity<>(row.getId().toString(), HttpStatus.OK);
+            return new ResponseEntity<>(url + row.getId().toString(), HttpStatus.OK);
         }
         Code c = new Code();
         c.setContent(code);
         c.setDigest(digest);
         Code sc = codeRepository.save(c);
-        return new ResponseEntity<>(sc.getId().toString(), HttpStatus.OK);
+        return new ResponseEntity<>(url + sc.getId().toString(), HttpStatus.OK);
     }
 }
